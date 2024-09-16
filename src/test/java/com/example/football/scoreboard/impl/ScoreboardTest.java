f2f206ee923ddfaaa2aaae6ce79ece2c66ca23e8
Mahaboob Monutty Veettilayil <MahaboobMonutty.Veettilayil@mastercardpaymentservices.com>
2024-09-16 16:30:08 +0200

    test: add tests for match retrievel based on match Id and exception handling
    - Match retrieval handling based on valid match Id
    - Exception handling with custom exception for invalid match Id
    - Exception handling for null or empty match Id

--- src/test/java/com/example/football/scoreboard/impl/ScoreboardTest.java
+++ src/test/java/com/example/football/scoreboard/impl/ScoreboardTest.java
@@ -1,6 +1,8 @@
 package com.example.football.scoreboard.impl;
 
+import com.example.football.scoreboard.Match;
 import com.example.football.scoreboard.MatchStorage;
+import com.example.football.scoreboard.exception.MatchNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,8 +11,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -43,5 +48,62 @@ public class ScoreboardTest {
                 match.isLive()
         ));
 
+    }
+
+    @Test
+    void testGetMatchById_ValidId() {
+        // Arrange
+        String matchId = "match1";
+        String homeTeam = "Team A";
+        String awayTeam = "Team B";
+        LocalDateTime now = LocalDateTime.now();
+
+        Match match = new Match(homeTeam, awayTeam, 1 ,  1, now);
+
+        when(matchStorage.findMatch((matchId))).thenReturn(match);
+
+        // Act
+        Match retrievedMatch = scoreboard.getMatch(matchId);
+
+        // Assert
+        assertEquals(homeTeam, retrievedMatch.getHomeTeam());
+        assertEquals(awayTeam, retrievedMatch.getAwayTeam());
+        assertEquals(1, retrievedMatch.getHomeTeamScore());
+        assertEquals(1, retrievedMatch.getAwayTeamScore());
+    }
+
+    @Test
+    void testGetMatchById_InvalidId() {
+        // Arrange
+        String matchId = "invalid1";
+
+        when(matchStorage.findMatch((matchId))).thenReturn(null);
+
+        // Act & Assert
+        MatchNotFoundException exception = assertThrows(MatchNotFoundException.class, () -> scoreboard.getMatch(matchId));
+
+        // Assert the exception message
+        assertEquals("No match found with ID: invalid1", exception.getMessage());
+    }
+
+    @Test
+    void testGetMatchById_NullOrEmptyId() {
+        // Act & Assert
+        IllegalArgumentException nullException = assertThrows(IllegalArgumentException.class, () -> scoreboard.getMatch(null));
+
+        // Assert the exception message
+        assertEquals("Match ID cannot be null or empty", nullException.getMessage());
+
+        // Act & Assert
+        IllegalArgumentException emptyException = assertThrows(IllegalArgumentException.class, () -> scoreboard.getMatch(""));
+
+        // Assert the exception message
+        assertEquals("Match ID cannot be null or empty", emptyException.getMessage());
+
+        // Act & Assert
+        IllegalArgumentException spaceException = assertThrows(IllegalArgumentException.class, () -> scoreboard.getMatch(" "));
+
+        // Assert the exception message
+        assertEquals("Match ID cannot be null or empty", spaceException.getMessage());
     }
 }
