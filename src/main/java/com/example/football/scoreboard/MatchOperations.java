235922bfc5e0530590f206a8bc1cdd91fc968138
Mahaboob Monutty Veettilayil <MahaboobMonutty.Veettilayil@mastercardpaymentservices.com>
2024-09-16 12:57:15 +0200

    Refactor: updated MatchOperations interface and scoreBoard implementations

--- src/main/java/com/example/football/scoreboard/MatchOperations.java
+++ src/main/java/com/example/football/scoreboard/MatchOperations.java
@@ -1,8 +1,16 @@
 package com.example.football.scoreboard;
 
+import java.util.List;
+
 public interface MatchOperations {
+
+    void startMatch(String homeTeam, String awayTeam);
+
+    Match getMatch(Match match);
 
     void updateMatchScore(int scoreA, int scoreB);
 
     void finishMatch();
+
+    List<Match> getMatchSummary();
 }
--- src/main/java/com/example/football/scoreboard/impl/Scoreboard.java
+++ src/main/java/com/example/football/scoreboard/impl/Scoreboard.java
@@ -1,8 +1,28 @@
 package com.example.football.scoreboard.impl;
 
+import com.example.football.scoreboard.Match;
 import com.example.football.scoreboard.MatchOperations;
+import com.example.football.scoreboard.MatchStorage;
+
+import java.util.List;
 
 public class Scoreboard implements MatchOperations {
+
+    private final MatchStorage matchStorage;
+
+    public Scoreboard(MatchStorage matchStorage) {
+        this.matchStorage = matchStorage;
+    }
+
+
+    @Override
+    public void startMatch(String homeTeam, String awayTeam) {
+    }
+
+    @Override
+    public Match getMatch(Match match) {
+        return null;
+    }
 
     @Override
     public void updateMatchScore(int scoreA, int scoreB) {
@@ -12,5 +32,10 @@ public class Scoreboard implements MatchOperations {
     @Override
     public void finishMatch() {
 
+    }
+
+    @Override
+    public List<Match> getMatchSummary() {
+        return null;
     }
 }
